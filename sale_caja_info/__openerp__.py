fdb8a044b6783f2383ae21bffdcc88470ec5e61a
root <root@eagle897.dedicatedpanel.com>
2016-11-14 17:22:25 -0600

    [REF] sale_caja_info first time added.

---
+++ sale_caja_info/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+
+ 
+{
+    'name' : 'Info de Caja en vista de Cotizacion y Pedido de Venta Oisa',
+    'version' : '1.0',
+    'author' : 'silvau',
+    'website' : 'http://www.zeval.com.mx',
+    'category' : '',
+    'depends' : ['sale','oisa'],
+    'description': """
+    Agrega informacion de la caja cotizada en la vista de forma
+    """,
+    'data': [
+        'views/sale_order_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'images': [],
+}
---
+++ sale_caja_info/sale.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+import pdb
+
+class sale_order(osv.osv):
+    _inherit = "sale.order"
+
+
+    def _get_caja_id(self, cr, uid, ids, field_name, arg, context):
+        so_line_obj=self.pool.get('sale.order.line')
+        product_obj=self.pool.get('product.product')
+        product_cajas_ids = product_obj.search(cr,uid,[('es_caja','=',True)])
+        res={}
+        for i in ids:
+            cajas=so_line_obj.search(cr,uid,[('product_id','in',product_cajas_ids), ('order_id','=',i)],count=True)
+            if cajas > 0 :
+                res[i] = so_line_obj.search(cr,uid,[('product_id','in',product_cajas_ids), ('order_id','=',i)],limit=1)
+            else:
+                res[i] = False
+        return res
+
+    _columns = {
+        'caja_id': fields.function(_get_caja_id, type='many2one', obj='sale.order.line', method='True',  string='Caja'),
+        'cotizacion_caja_id': fields.related('caja_id','product_id','cot_id',type='many2one', relation='oisa.cotizacion',string='Cotizacion de la Caja'),
+        'medidas_cotizacion_caja_id': fields.related('caja_id','product_id','cot_id','medidas',type='one2many', relation='oisa.cotizacion.medidas',string='Medidas de la Caja'),
+        'wc_cotizacion_caja': fields.related('caja_id','product_id','cot_id','wc',type='float', relation='oisa.cotizacion',string='WC'),
+        'wi_cotizacion_caja': fields.related('caja_id','product_id','cot_id','wi',type='float', relation='oisa.cotizacion',string='WI'),
+        'uc_cotizacion_caja': fields.related('caja_id','product_id','cot_id','uc',type='integer', relation='oisa.cotizacion',string='UC'),
+        'ui_cotizacion_caja': fields.related('caja_id','product_id','cot_id','ui',type='integer', relation='oisa.cotizacion',string='UI'),
+        'chk_flauta_cotizacion_caja': fields.related('caja_id','product_id','cot_id','chk_flauta',type='boolean', relation='oisa.cotizacion',string='(Lleva flauta?)'),
+        'chk_laminado_cotizacion_caja': fields.related('caja_id','product_id','cot_id','chk_laminado',type='boolean', relation='oisa.cotizacion',string='(Lleva Laminado?)'),
+        'material_cotizacion_caja_id': fields.related('caja_id','product_id','cot_id','material_id',type='many2one', relation='product.product',string='Material'),
+        'procesos_cotizacion_caja_id': fields.related('caja_id','product_id','cot_id','proceso_ids',type='one2many', relation='oisa.cot.proceso.ins',string='Procesos de la Caja'),
+        'chk_guias_cotizacion_caja': fields.related('caja_id','product_id','cot_id','chk_guias',type='boolean', relation='oisa.cotizacion',string='Guias'),
+        'tipo_tabla_cotizacion_caja': fields.related('caja_id','product_id','cot_id','tipo_tabla',type='selection', relation='oisa.cotizacion',string='Tabla'),
+        'ean13_cotizacion_caja': fields.related('caja_id','product_id','ean13',type='char', relation='product.product',string='Código EAN13'),
+    }
