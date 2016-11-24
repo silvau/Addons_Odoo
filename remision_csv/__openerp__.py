e6767bf6d695ab9637407896d2021725e1d95127
root <root@eagle897.dedicatedpanel.com>
2016-11-24 12:17:30 -0600

    [REF] remision_csv first time added.

---
+++ remision_csv/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+
+ 
+{
+    'name' : 'Export Remision CSV',
+    'version' : '1.0',
+    'author' : 'silvau',
+    'website' : 'http://www.zeval.com.mx',
+    'category' : '',
+    'depends' : ['stock'],
+    'description': """
+    Genera un archivo csv a partir de la remision de entrada
+    """,
+    'data': [
+        'wizard/download_csv_view.xml',
+        'views/stock_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'images': [],
+}
